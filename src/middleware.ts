b3ada97668c56445806b913b6efa421786700750
Lt. Wilson <riley@ltwilson.tv>
2025-07-25 12:34:58 -0400

    Add middleware redirects for social media

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,5 +13,19 @@ export const onRequest: MiddlewareHandler = async (context, next) => {
     return context.redirect('https://vods.ltwilson.tv', 301)
   }
 
+  // social media redirects
+  switch (url.pathname) {
+    case '/youtube':
+      return context.redirect('https://youtube.com/@theltwilson', 301)
+    case '/twitch':
+      return context.redirect('https://twitch.tv/theltwilson', 301)
+    case '/discord':
+      return context.redirect('https://discord.gg/fJtyxttGpq', 301)
+    case '/github':
+      return context.redirect('https://github.com/theltwilson', 301)
+    default:
+      break;
+  }
+  
   return next()
 }
