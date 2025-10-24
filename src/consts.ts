e8b4cd09fc2a9a0f65afac2b6a1ca7802c51cefd
Lt. Wilson <riley@ltwilson.tv>
2025-10-24 17:10:12 -0400

    Removed mentions of Lt. Wilson

--- src/consts.ts
+++ src/consts.ts
@@ -15,20 +15,20 @@ export type Socials = {
 }
 
 export const SITE: Site = {
-  TITLE: 'Lt. Wilson',
+  TITLE: 'rcw.lol',
   DESCRIPTION:
     'Personal website of a guy that edits videos and occasionally dabbles in coding projects.',
   EMAIL: 'contact@ltwilson.tv',
   NUM_POSTS_ON_HOMEPAGE: 7,
   POSTS_PER_PAGE: 8,
   VODS_PER_PAGE: 15,
-  SITEURL: 'https://ltwilson.tv',
+  SITEURL: 'https://rcw.lol',
 }
 
 export const SOCIALS: Socials[] = [
-  { TITLE: 'YouTube', ICON: 'lucide:youtube', URL: 'https://youtube.com/@theltwilson' },
-  { TITLE: 'Twitch', ICON: 'lucide:twitch', URL: 'https://twitch.tv/theltwilson' },
-  { TITLE: 'Bluesky', ICON: 'mingcute:bluesky-social-line', URL: 'https://bsky.app/profile/ltwilson.tv' },
+  { TITLE: 'YouTube', ICON: 'lucide:youtube', URL: 'https://youtube.com/@rcwowo' },
+  { TITLE: 'Twitch', ICON: 'lucide:twitch', URL: 'https://twitch.tv/rcwowo' },
+  { TITLE: 'Bluesky', ICON: 'mingcute:bluesky-social-line', URL: 'https://bsky.app/profile/rcw.lol' },
   { TITLE: 'Discord', ICON: 'mingcute:discord-line', URL: 'https://discord.gg/fJtyxttGpq' },
-  { TITLE: 'GitHub', ICON: 'lucide:github', URL: 'https://github.com/theltwilson'}
+  { TITLE: 'Tangled', ICON: 'lucide:git-graph', URL: 'https://tangled.org/@rcw.lol'}
 ]
